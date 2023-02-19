4f8d91e61664e456a43e2c35851809fb9f8586e5
이예서 <49704047+yeseoLee@users.noreply.github.com>
2023-02-19 14:26:44 +0900

    질문 작업 - DB, Query

--- board/domain/question.go
+++ board/domain/question.go
@@ -1,33 +1,67 @@
 package domain
 
-// input(DTO)
+import "time"
 
-// output(VO)
-
-// DBStruct(DAO)
+// Entity
 type Question struct {
-	Id         int64    `json:"id"`
-	Title      string   `json:"title"`
-	Content    string   `json:"body"`
-	Writer     string   `json:"writer"`
-	WriterId   string   `json:"writerId"`
-	Images     []string `json:"images"`
-	CreateTime string   `json:"createTime"`
-	UpdateTime string   `json:"updateTime"`
+	Id        uint      `json:"id"`
+	WriterId  string    `json:"writerId"`
+	Title     string    `json:"title"`
+	Content   string    `json:"body"`
+	Tags      []string  `json:"tags"`
+	Images    []string  `json:"images"`
+	IsAccept  bool      `json:"IsAccept"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	DeletedAt time.Time `json:"deleteddAt"`
+	// ---------
+	// 좋아요 수
+	// 조회 수
+}
+
+// DTO
+type QuestionInput struct {
+	WriterId string   `json:"writerId"`
+	Title    string   `json:"title"`
+	Content  string   `json:"body"`
+	Tags     []string `json:"tags"`
+	Images   []string `json:"images"`
+}
+
+type QuestionOutput struct {
+	Id        uint      `json:"id"`
+	WriterId  string    `json:"writer"`
+	Title     string    `json:"title"`
+	Content   string    `json:"body"`
+	Tags      []string  `json:"tags"`
+	Images    []string  `json:"images"`
+	IsAccept  bool      `json:"IsAccept"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+type QuestionsearchOption struct {
+	Title    string `json:"title"`
+	Content  string `json:"body"`
+	WriterId string `json:"writerId"`
+}
+
+type QuestionOrderOption struct {
 }
 
 type QuestionRepository interface {
-	GetByID(id int64) (*Question, error)
-	Fetch(offset, limit int) ([]*Question, error)
-	Create(question *Question) (*Question, error)
-	Update(id int64, question *Question) (*Question, error)
-	Delete(id int64) error
+	FindById(id uint) (*Question, error)
+	FindAll(limit, offset int) ([]*Question, error)
+	//FindAllByTags(tags []string) ([]*Question, error)
+	Create(questionInput *QuestionInput) (*Question, error)
+	Update(id uint, questionUpdate map[string]interface{}) (*Question, error)
+	Delete(id uint) error
 }
 
 type QuestionUseCase interface {
-	GetByID(id int64) (res *Question, err error)
-	Fetch(offset, limit int) (res []*Question, err error)
-	Create(question *Question) (*Question, error)
-	Update(id int64, question *Question) (*Question, error)
-	Delete(id int64) error
+	Get(id uint) (*QuestionOutput, error)
+	GetAll(limit, offset int) ([]*QuestionOutput, error)
+	Create(questionInput *QuestionInput) (*QuestionOutput, error)
+	Edit(WriterId string, id uint, questionEdit map[string]interface{}) (*QuestionOutput, error)
+	Accept(WriterId string, id uint) error
+	Delete(WriterId string, id uint) error
 }
--- board/question/repository/question_repository.go
+++ board/question/repository/question_repository.go
@@ -2,7 +2,8 @@ package question
 
 import (
 	"database/sql"
-	"time"
+	"errors"
+	"fmt"
 
 	"board/datasource"
 	model "board/domain"
@@ -20,33 +21,77 @@ type QuestionRepository struct {
 	DBEngine *sql.DB
 }
 
-func (r *QuestionRepository) GetByID(id int64) (*model.Question, error) {
-	//r.DBEngine
-	return &model.Question{}, nil
+func (r *QuestionRepository) FindById(id uint) (*model.Question, error) {
+	var q model.Question
+
+	row := r.DBEngine.QueryRow("SELECT id, title, content, writerId, images, createdAt,updatedAt FROM tbQuestion WHERE id = ?", id)
+	err := row.Scan(&q.Id, &q.Title, &q.Content, &q.WriterId, &q.Images, &q.CreatedAt, &q.UpdatedAt)
+	switch {
+	case err == sql.ErrNoRows:
+		return nil, errors.New("no data")
+	case err != nil:
+		return nil, errors.New("query error")
+	}
+	return &q, nil
 }
 
-func (r *QuestionRepository) Fetch(offset, limit int) ([]*model.Question, error) {
-	return []*model.Question{}, nil
+func (r *QuestionRepository) FindAll(limit, offset int) ([]*model.Question, error) {
+	var qList []*model.Question
+
+	rows, err := r.DBEngine.Query("SELECT id, title, content, writerId, images, createdAt,updatedAt FROM tbQuestion LIMIT ? OFFSET ?", limit, offset)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var q model.Question
+		rows.Scan(&q.Id, &q.Title, &q.Content, &q.WriterId, &q.Images, &q.CreatedAt, &q.UpdatedAt)
+		qList = append(qList, &q)
+	}
+
+	return qList, nil
 }
 
-func (r *QuestionRepository) Create(question *model.Question) (*model.Question, error) {
-	question.UpdateTime = time.Now().Format("2006-01-02 15:04:05")
-	result, err := r.DBEngine.Exec("INSERT INTO `tbQuestion`(`Title`,`Content`,`Writer`,`WriterId`,`Images`,`CreateTime`) VALUES (?, ?, ?, ?, ?, ?)",
-		question.Title, question.Content, question.Writer, question.WriterId, question.Images, question.CreateTime)
+func (r *QuestionRepository) Create(questionInput *model.QuestionInput) (*model.Question, error) {
+	var question *model.Question
+	result, err := r.DBEngine.Exec("INSERT INTO `tbQuestion`(`Title`,`Content`,`WriterId`,`Images`,`CreatedAt`) VALUES (?, ?, ?, ?, now())",
+		questionInput.Title, questionInput.Content, questionInput.WriterId, questionInput.Images)
 	if err != nil {
 		return question, err
 	}
-	question.Id, err = result.LastInsertId()
+
+	id, err := result.LastInsertId()
+	question.Id = uint(id)
 	if err != nil {
 		return question, err
 	}
 	return question, nil
 }
 
-func (r *QuestionRepository) Update(id int64, question *model.Question) (*model.Question, error) {
-	return &model.Question{}, nil
+func (r *QuestionRepository) Update(id uint, questionUpdate map[string]interface{}) (*model.Question, error) {
+	// TODO: map key-value check & make query logic
+	var question *model.Question
+	_, err := r.DBEngine.Exec("UPDATE tbQuestion SET Title = ?, Content = ?, Images = ?, updatedAt = now() WHERE id = ?",
+		questionUpdate["Title"], questionUpdate["Content"], questionUpdate["Images"])
+	if err != nil {
+		return question, err
+	}
+	return question, nil
 }
 
-func (r *QuestionRepository) Delete(id int64) error {
-	return nil
+func (r *QuestionRepository) Delete(id uint) error {
+	result, err := r.DBEngine.Exec("DELETE FROM `tbQuestion` WHERE id = ?", id)
+	if err != nil {
+		return err
+	}
+
+	rows, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rows != 1 {
+		return fmt.Errorf("expected to affect 1 row, affected %d", rows)
+	}
+	return err
 }
